452b40e790f8b8d55f4b99cf2de05edc73e82fc0
fpofinet <lionelkanon67@gmail.com>
2020-11-11 21:54:03 +0100

    template finale

--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Utilisateurs;
+use Doctrine\DBAL\Types\StringType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class InscriptionType extends AbstractType
